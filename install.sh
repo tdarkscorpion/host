927e5c6df1776370431d2a3c6bfd26d52a72d24a
DarkScorpion <31767129+tdarkscorpion@users.noreply.github.com>
2026-05-10 03:12:32 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -178,16 +178,31 @@ if [ ! -f "$CP_DIR/step5" ]; then
     if [ "$OS" == "ubuntu" ]; then
         # SSL Config
         cp ./ssl-httpd.conf /etc/apache2/sites-available/default-ssl.conf
+        sed -i "s/www.talismanofcouragev2.online/$AUTH_DOMAIN/g" /etc/apache2/sites-available/default-ssl.conf
+        sed -i "s/patch.talismanofcouragev2.online/patch.$AUTH_DOMAIN/g" /etc/apache2/sites-available/default-ssl.conf
+        
         a2enmod ssl
+        a2enmod rewrite
         a2ensite default-ssl
         # Port 80/81 Config
         cp ./ports-80-81.conf /etc/apache2/sites-available/ports.conf
         a2ensite ports
+        
+        # Enable .htaccess support
+        sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+        
         systemctl restart apache2
     else
         # Configs in conf.d
         cp ./ssl-httpd.conf /etc/httpd/conf.d/
+        sed -i "s/www.talismanofcouragev2.online/$AUTH_DOMAIN/g" /etc/httpd/conf.d/ssl-httpd.conf
+        sed -i "s/patch.talismanofcouragev2.online/patch.$AUTH_DOMAIN/g" /etc/httpd/conf.d/ssl-httpd.conf
+        
         cp ./ports-80-81.conf /etc/httpd/conf.d/
+        
+        # Enable .htaccess support
+        sed -i '/<Directory "\/var\/www\/html">/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf
+        
         systemctl restart httpd
     fi
     touch "$CP_DIR/step5"
@@ -256,6 +271,14 @@ if [ ! -f "$CP_DIR/step7" ]; then
     touch "$CP_DIR/step7"
 fi
 
+# Restart Web Server
+log "Restarting Web Server to apply all changes..."
+if [ "$OS" == "ubuntu" ]; then
+    systemctl restart apache2
+else
+    systemctl restart httpd
+fi
+
 log "==============================================="
 log "   INSTALLATION COMPLETED SUCCESSFULLY!        "
 log "==============================================="
@@ -265,4 +288,4 @@ log "MySQL Root Password: $DB_PASS"
 log "-----------------------------------------------"
 log "Anti-DDoS: Active and running."
 log "-----------------------------------------------"
-echo "Please restart your server or run: sudo systemctl restart apache2 mysqld"
+echo "Web server restarted. If you encounter issues, you can restart it manually."
